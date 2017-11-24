62dfbd28f2d2e222a23014b6151a27b3a0f512bf
NGI <ngarcia@umanis.com>
2017-11-24 09:40:51 +0100

    Gestion date

--- pages/paypal.php
+++ pages/paypal.php
@@ -1,8 +1,18 @@
 <?php
+    use App\Table\Commande;
     $idClient = $_SESSION["client"]->id;
-    $horaire = $_POST["horaire"];
+    $details = $_SESSION["order"];
     $total = $_POST["total"];
     $money =  str_replace(".",",",number_format($total,2))." €";
+
+    $horaire = $_POST["horaire"];
+    $horaire = explode(":",$horaire);
+    $date = new DateTime();
+    $date->setTime($horaire[0],$horaire[1]);
+
+    //var_dump($date);
+
+    //Commande::insert($idClient,$horaire,$total,$details);
  ?>
 
 Paiement par paypal
