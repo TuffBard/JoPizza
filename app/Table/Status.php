016ae72815119208a6b1c7383032ca1c94d96253
NGI <ngarcia@umanis.com>
2017-11-29 17:50:21 +0100

    add comments

--- app/Table/Status.php
+++ app/Table/Status.php
@@ -1,7 +1,15 @@
 <?php
 namespace App\Table;
 
+/**
+ * Class contenant la liste des status de commande
+ */
 class Status {
+    /**
+     * Renvoi un status en fonction de son id
+     * @param  Int $id Id du status
+     * @return String Libelle du status
+     */
     public static function getById($id){
         switch ($id) {
             case 1:
