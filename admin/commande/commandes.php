fafeac97c42db5df9cadf097ce081fdcc388da55
NGI <ngarcia@umanis.com>
2017-12-06 16:34:05 +0100

    Admin - Status

--- admin/commande/commandes.php
+++ admin/commande/commandes.php
@@ -8,7 +8,7 @@
                 <h4 class="card-title">Les commandes</h4>
             </div>
             <div class="card-body">
-                <!-- <a class="btn btn-success float-right mb-md-3" href="admin.php?r=commande&p=ajoutCommande"><span class="oi oi-plus mr-auto"></span> <b>Ajouter</b></a> -->
+                <a class="btn btn-success float-right mb-md-3" href="admin.php?r=commande&p=ajoutCommande"><span class="oi oi-plus mr-auto"></span> <b>Ajouter</b></a>
                 <table class="list-commande table" width="100%">
                     <thead>
                         <tr>
@@ -17,6 +17,7 @@
                             <th>Contenu</th>
                             <th>Total</th>
                             <th>Statut</th>
+                            <th>Actions</th>
                         </tr>
                     </thead>
                 </table>
@@ -24,3 +25,12 @@
         </div>
     </div>
 </div>
+
+<script id="template-status" type="x-tmpl-mustache">
+<label class='text-{{color}}'><span class='oi {{icon}}'></span> {{status}}</label>
+</script>
+
+<script id="template-actions" type="x-tmpl-mustache">
+    <button class="btn btn-sm btn-primary"><span class='oi oi-pencil'></span></button>
+    <button class="btn btn-sm btn-danger"><span class='oi oi-x'></span></button>
+</script>
--- public/js/admin/commandes.js
+++ public/js/admin/commandes.js
@@ -25,29 +25,91 @@ function initDatatable() {
             { data: "status" }
         ],
         columnDefs: [{
-                targets: 0,
-                render: function(data, type, row) {
-                    return moment(data).format("DD/MM/YYYY <b>HH:mm</b>");
-                }
-            },
-            {
-                targets: 1,
-                render: function(data, type, row) {
-                    return data.nom + " " + data.prenom
-                }
-            },
-            {
-                targets: 2,
-                render: function(data, type, row) {
-                    return "<table width='100%'>" + data.map(d => "<tr><td>" + d.pizza.libelle + "</td><td>" + d.quantity + "</td></tr>").join("") + "</table>";
-                }
-            },
-            {
-                targets: 3,
-                render: function(data, type, row) {
-                    return parseFloat(data).toFixed(2).replace(".", ",") + " €";
-                }
+            targets: 0,
+            render: function(data, type, row) {
+                return moment(data).format("DD/MM/YYYY <b>HH:mm</b>");
             }
-        ]
-    });
+        },
+        {
+            targets: 1,
+            render: function(data, type, row) {
+                return data.nom + " " + data.prenom
+            }
+        },
+        {
+            targets: 2,
+            render: function(data, type, row) {
+                return "<table width='100%'>" + data.map(d => "<tr><td>" + d.pizza.libelle + "</td><td>" + d.quantity + "</td></tr>").join("") + "</table>";
+            }
+        },
+        {
+            targets: 3,
+            render: function(data, type, row) {
+                return parseFloat(data).toFixed(2).replace(".", ",") + " €";
+            }
+        },
+        {
+            targets: 4,
+            render: function(data, type, row) {
+                let template = $("#template-status").html();
+                let values = getValues(data);
+
+                return Mustache.render(template, values);
+            }
+        },
+        {
+            targets: 5,
+            render: function(data, type, row){
+                let template = $("#template-actions").html();
+                let values = {};
+                return Mustache.render(template, values);
+            }
+        }
+    ]
+});
+}
+
+function getValues(status){
+    switch (status) {
+        case "En attente de paiement":
+            return {
+                status: status,
+                icon: "oi-timer",
+                color: "warning"
+            };
+        case "Paiement validé":
+            return {
+                status: status,
+                icon: "oi-credit-card",
+                color: "info"
+            };
+        case "En préparation":
+            return {
+                status: status,
+                icon: "oi-timer",
+                color: "primary"
+            };
+        case "Commande prête":
+            return {
+                status: status,
+                icon: "oi-check",
+                color: "success"
+            }
+        case "Commande terminée":
+            return {
+                status: status,
+                icon: "oi-thumb-up",
+                color: "muted"
+            }
+        case "Commande annulée":
+            return {
+                status: status,
+                icon: "oi-x",
+                color: "danger"
+            }
+        default:
+            return {
+                status: status
+            };
+    }
 }
